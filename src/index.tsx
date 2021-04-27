707c3a13073a01bf0023d0cc40c2574d7a852e11
hornta <hornta@protonmail.com>
2021-04-27 23:11:32 +0200

    attribution

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
 import { App } from "./app";
+import tmdbLogo from "./tmdb-logo.svg";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -13,6 +14,15 @@ ReactDOM.render(
 				<App />
 			</BrowserRouter>
 		</Provider>
+		<a
+			href="https://www.themoviedb.org/"
+			target="_blank"
+			className="data-provider-attribution"
+			rel="noreferrer"
+			title="Opens in new window"
+		>
+			Data from <img loading="lazy" alt="TMDB Logotype" src={tmdbLogo} />
+		</a>
 	</React.StrictMode>,
 	document.querySelector("#root")
 );
